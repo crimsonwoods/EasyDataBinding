3ec8cc4114639628311527201417be1294532bd3
crimsonwoods <crimsonwoods.dev@gmail.com>
2021-02-23 16:33:55 +0900

    Add new test cases for "Res" type

--- lib/src/test/java/net/crimsonwoods/easydatabinding/binding/TextViewBindingTest.kt
+++ lib/src/test/java/net/crimsonwoods/easydatabinding/binding/TextViewBindingTest.kt
@@ -9,12 +9,37 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import kotlin.test.Test
+import net.crimsonwoods.easydatabinding.R
 import net.crimsonwoods.easydatabinding.fragment.TestFragment
 import net.crimsonwoods.easydatabinding.models.Text
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class TextViewBindingTest {
+    @Test
+    fun testBinding_setText_for_Res() {
+        val scenario = launchFragmentInContainer<TestFragment>()
+            .moveToState(Lifecycle.State.RESUMED)
+        onView(withId(android.R.id.text1)).check(ViewAssertions.matches(withText("")))
+        scenario.onFragment { fragment ->
+            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+                .setText(Text.of(android.R.string.ok))
+        }
+        onView(withId(android.R.id.text1)).check(ViewAssertions.matches(withText("OK")))
+    }
+
+    @Test
+    fun testBinding_setText_for_ResWithArgs() {
+        val scenario = launchFragmentInContainer<TestFragment>()
+            .moveToState(Lifecycle.State.RESUMED)
+        onView(withId(android.R.id.text1)).check(ViewAssertions.matches(withText("")))
+        scenario.onFragment { fragment ->
+            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+                .setText(Text.of(R.string.character_counter_pattern, 1, 2))
+        }
+        onView(withId(android.R.id.text1)).check(ViewAssertions.matches(withText("1/2")))
+    }
+
     @Test
     fun testBinding_setText_for_CharSequence() {
         val scenario = launchFragmentInContainer<TestFragment>()
