df79f8fa4d97c7a7bf6f69b86644a4e40deaaedf
crimsonwoods <crimsonwoods.dev@gmail.com>
2021-02-23 16:40:23 +0900

    Add new test cases for "Multilingual"

--- lib/src/test/java/net/crimsonwoods/easydatabinding/binding/TextViewBindingTest.kt
+++ lib/src/test/java/net/crimsonwoods/easydatabinding/binding/TextViewBindingTest.kt
@@ -8,11 +8,13 @@ import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import java.util.Locale
 import kotlin.test.Test
 import net.crimsonwoods.easydatabinding.R
 import net.crimsonwoods.easydatabinding.fragment.TestFragment
 import net.crimsonwoods.easydatabinding.models.Text
 import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
 class TextViewBindingTest {
@@ -50,5 +52,57 @@ class TextViewBindingTest {
                 .setText(Text.of("test"))
         }
         onView(withId(android.R.id.text1)).check(ViewAssertions.matches(withText("test")))
+    }
+
+    @Test
+    @Config(qualifiers = "ja-rJP")
+    fun testBinding_setText_for_Multilingual_jaJP() {
+        val scenario = launchFragmentInContainer<TestFragment>()
+            .moveToState(Lifecycle.State.RESUMED)
+        onView(withId(android.R.id.text1)).check(ViewAssertions.matches(withText("")))
+        scenario.onFragment { fragment ->
+            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+                .setText(Text.of(Locale.JAPAN to "テスト", Locale.US to "Test"))
+        }
+        onView(withId(android.R.id.text1)).check(ViewAssertions.matches(withText("テスト")))
+    }
+
+    @Test
+    @Config(qualifiers = "en-rUS")
+    fun testBinding_setText_for_Multilingual_jaJP_Fallback() {
+        val scenario = launchFragmentInContainer<TestFragment>()
+            .moveToState(Lifecycle.State.RESUMED)
+        onView(withId(android.R.id.text1)).check(ViewAssertions.matches(withText("")))
+        scenario.onFragment { fragment ->
+            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+                .setText(Text.of(mapOf(Locale.JAPAN to "テスト"), "test"))
+        }
+        onView(withId(android.R.id.text1)).check(ViewAssertions.matches(withText("test")))
+    }
+
+    @Test
+    @Config(qualifiers = "en-rUS")
+    fun testBinding_setText_for_Multilingual_jaJP_FallbackRes() {
+        val scenario = launchFragmentInContainer<TestFragment>()
+            .moveToState(Lifecycle.State.RESUMED)
+        onView(withId(android.R.id.text1)).check(ViewAssertions.matches(withText("")))
+        scenario.onFragment { fragment ->
+            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+                .setText(Text.of(mapOf(Locale.JAPAN to "テスト"), android.R.string.ok))
+        }
+        onView(withId(android.R.id.text1)).check(ViewAssertions.matches(withText("OK")))
+    }
+
+    @Test
+    @Config(qualifiers = "en-rUS")
+    fun testBinding_setText_for_Multilingual_enUS() {
+        val scenario = launchFragmentInContainer<TestFragment>()
+            .moveToState(Lifecycle.State.RESUMED)
+        onView(withId(android.R.id.text1)).check(ViewAssertions.matches(withText("")))
+        scenario.onFragment { fragment ->
+            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+                .setText(Text.of(Locale.JAPAN to "テスト", Locale.US to "Test"))
+        }
+        onView(withId(android.R.id.text1)).check(ViewAssertions.matches(withText("Test")))
     }
 }
