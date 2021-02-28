0f2ed93ab6f51deb2f068772fd538abd49f6893a
crimsonwoods <crimsonwoods.dev@gmail.com>
2021-02-28 17:00:44 +0900

    Add new utility functions for "Image"

--- lib/src/main/java/net/crimsonwoods/easydatabinding/models/Image.kt
+++ lib/src/main/java/net/crimsonwoods/easydatabinding/models/Image.kt
@@ -21,4 +21,21 @@ sealed class Image {
     ) : Image()
 
     object None : Image()
+
+    companion object {
+        @JvmStatic
+        fun of(@DrawableRes resId: Int): Image = Res(resId)
+
+        @JvmStatic
+        fun of(drawable: android.graphics.drawable.Drawable?): Image = Drawable(drawable)
+
+        @JvmStatic
+        fun of(bitmap: android.graphics.Bitmap?): Image = Bitmap(bitmap)
+
+        @JvmStatic
+        fun of(uri: android.net.Uri?): Image = Uri(uri)
+
+        @JvmStatic
+        fun none(): Image = None
+    }
 }
