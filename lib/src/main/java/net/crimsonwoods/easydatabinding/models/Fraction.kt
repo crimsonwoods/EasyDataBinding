b21c15360eefdc3d919a08a6726da08381d7454f
crimsonwoods <crimsonwoods.dev@gmail.com>
2021-03-20 16:06:39 +0900

    Adds a new model "Fraction"

---
+++ lib/src/main/java/net/crimsonwoods/easydatabinding/models/Fraction.kt
@@ -0,0 +1,30 @@
+package net.crimsonwoods.easydatabinding.models
+
+import androidx.annotation.FractionRes
+
+sealed class Fraction {
+    data class Res(
+        @FractionRes val resId: Int,
+        val base: Int,
+        val parentBase: Int
+    ) : Fraction()
+
+    data class Float(
+        val rawValue: kotlin.Float
+    ) : Fraction()
+
+    companion object {
+        @JvmField
+        val ZERO: Fraction = Float(0f)
+
+        @JvmStatic
+        fun of(value: kotlin.Float): Fraction = Float(rawValue = value)
+
+        @JvmStatic
+        fun of(@FractionRes resId: Int, base: Int = 1, parentBase: Int = 1): Fraction = Res(
+            resId = resId,
+            base = base,
+            parentBase = parentBase
+        )
+    }
+}
