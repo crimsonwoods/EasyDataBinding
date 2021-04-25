5d1839f6c6ac0b1dc329fd32738e4c925c4f5173
crimsonwoods <crimsonwoods.dev@gmail.com>
2021-04-25 16:26:58 +0900

    Adds new BindingAdapters for AbsListView

---
+++ lib/src/main/java/net/crimsonwoods/easydatabinding/binding/AbsListView.kt
@@ -0,0 +1,25 @@
+package net.crimsonwoods.easydatabinding.binding
+
+import android.widget.AbsListView
+import androidx.databinding.BindingAdapter
+import net.crimsonwoods.easydatabinding.models.Bool
+
+@BindingAdapter("android:fastScrollEnabled")
+fun AbsListView.setFastScrollEnabled(value: Bool) {
+    isFastScrollEnabled = value.toBoolean()
+}
+
+@BindingAdapter("android:smoothScrollbar")
+fun AbsListView.setSmoothScrollbar(value: Bool) {
+    isSmoothScrollbarEnabled = value.toBoolean()
+}
+
+@BindingAdapter("android:stackFromBottom")
+fun AbsListView.setStackFromBottom(value: Bool) {
+    isStackFromBottom = value.toBoolean()
+}
+
+@BindingAdapter("android:textFilterEnabled")
+fun AbsListView.setTextFilterEnabled(value: Bool) {
+    isTextFilterEnabled = value.toBoolean()
+}
