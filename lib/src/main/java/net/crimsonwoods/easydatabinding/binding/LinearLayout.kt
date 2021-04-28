cea841689ec22c67be1178b19f6a982c85719b6e
crimsonwoods <crimsonwoods.dev@gmail.com>
2021-04-28 12:07:13 +0900

    Adds new BindingAdapters for LinearLayout

---
+++ lib/src/main/java/net/crimsonwoods/easydatabinding/binding/LinearLayout.kt
@@ -0,0 +1,28 @@
+package net.crimsonwoods.easydatabinding.binding
+
+import android.widget.LinearLayout
+import androidx.databinding.BindingAdapter
+import kotlin.math.roundToInt
+import net.crimsonwoods.easydatabinding.models.Bool
+import net.crimsonwoods.easydatabinding.models.Dimension
+import net.crimsonwoods.easydatabinding.models.Integer
+
+@BindingAdapter("android:baselineAligned")
+fun LinearLayout.setBaselineAligned(value: Bool) {
+    isBaselineAligned = value.toBoolean()
+}
+
+@BindingAdapter("android:baselineAlignedChildIndex")
+fun LinearLayout.setBaselineAlignedChildIndex(value: Integer) {
+    baselineAlignedChildIndex = value.toInt()
+}
+
+@BindingAdapter("android:measureWithLargestChild")
+fun LinearLayout.setMeasureWithLargestChild(value: Bool) {
+    isMeasureWithLargestChildEnabled = value.toBoolean()
+}
+
+@BindingAdapter("android:dividerPadding")
+fun LinearLayout.setDividerPadding(value: Dimension) {
+    dividerPadding = value.toPx().roundToInt()
+}
