ef397bfa106d335f24b93b323e6791f6fee465f0
crimsonwoods <crimsonwoods.dev@gmail.com>
2021-05-02 17:25:37 +0900

    Adds a new BindingAdapter of CompoundButton

--- lib/src/main/java/net/crimsonwoods/easydatabinding/binding/CompoundButton.kt
+++ lib/src/main/java/net/crimsonwoods/easydatabinding/binding/CompoundButton.kt
@@ -4,11 +4,21 @@ import android.widget.CompoundButton
 import androidx.core.widget.CompoundButtonCompat
 import androidx.databinding.BindingAdapter
 import net.crimsonwoods.easydatabinding.models.Bool
+import net.crimsonwoods.easydatabinding.models.Drawable
 import net.crimsonwoods.easydatabinding.models.Tint
 
 @BindingAdapter("android:checked")
 fun CompoundButton.setChecked(value: Bool) {
     isChecked = value.toBoolean()
+}
+
+@BindingAdapter("android:button")
+fun CompoundButton.setButtonDrawable(value: Drawable) {
+    if (value is Drawable.Res) {
+        setButtonDrawable(value.resId)
+    } else {
+        buttonDrawable = value.toDrawable()
+    }
 }
 
 @BindingAdapter("android:buttonTint")
