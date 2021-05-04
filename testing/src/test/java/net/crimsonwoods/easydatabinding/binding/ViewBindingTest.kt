70abb182fd41ad33804ad3a7013c827618466299
crimsonwoods <crimsonwoods.dev@gmail.com>
2021-05-04 17:44:14 +0900

    Uses View for testing instead of TextView

--- testing/src/test/java/net/crimsonwoods/easydatabinding/binding/ViewBindingTest.kt
+++ testing/src/test/java/net/crimsonwoods/easydatabinding/binding/ViewBindingTest.kt
@@ -7,7 +7,6 @@ import android.graphics.drawable.Drawable
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.RectShape
 import android.view.View
-import android.widget.TextView
 import androidx.annotation.ColorInt
 import androidx.annotation.Px
 import androidx.fragment.app.testing.FragmentScenario
@@ -54,103 +53,103 @@ class ViewBindingTest {
     @Test
     fun testBinding_setAlpha_Res_Base() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setAlpha(Fraction.of(R.fraction.test_fraction, 1, 2))
         }
-        onView(withId(android.R.id.text1)).check(matches(withAlpha(0.5f)))
+        onView(withId(R.id.border)).check(matches(withAlpha(0.5f)))
     }
 
     @Test
     fun testBinding_setAlpha_Res_ParentBase() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setAlpha(Fraction.of(R.fraction.test_fraction_p, 2, 1))
         }
-        onView(withId(android.R.id.text1)).check(matches(withAlpha(0.5f)))
+        onView(withId(R.id.border)).check(matches(withAlpha(0.5f)))
     }
 
     @Test
     fun testBinding_setAlpha_Float() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setAlpha(Fraction.of(0.1f))
         }
-        onView(withId(android.R.id.text1)).check(matches(withAlpha(0.1f)))
+        onView(withId(R.id.border)).check(matches(withAlpha(0.1f)))
     }
 
     @Test
     fun testBinding_setBackground_Color_Int() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setBackground(Background.Color.Int(0xff))
         }
-        onView(withId(android.R.id.text1)).check(matches(hasBackgroundColor(0xff)))
+        onView(withId(R.id.border)).check(matches(hasBackgroundColor(0xff)))
     }
 
     @Test
     fun testBinding_setBackground_Color_Res() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setBackground(Background.Color.Res(android.R.color.white))
         }
-        onView(withId(android.R.id.text1)).check(matches(hasBackgroundColor(Color.WHITE)))
+        onView(withId(R.id.border)).check(matches(hasBackgroundColor(Color.WHITE)))
     }
 
     @Test
     fun testBinding_setBackground_Color_String() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setBackground(Background.Color.String("blue"))
         }
-        onView(withId(android.R.id.text1)).check(matches(hasBackgroundColor(Color.BLUE)))
+        onView(withId(R.id.border)).check(matches(hasBackgroundColor(Color.BLUE)))
     }
 
     @Test
     fun testBinding_setBackground_Color_Drawable() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setBackground(Background.Color.Drawable(ColorDrawable(Color.YELLOW)))
         }
-        onView(withId(android.R.id.text1)).check(matches(hasBackgroundColor(Color.YELLOW)))
+        onView(withId(R.id.border)).check(matches(hasBackgroundColor(Color.YELLOW)))
     }
 
     @Test
     fun testBinding_setBackground_Res() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setBackground(Background.Res(android.R.drawable.screen_background_dark))
         }
-        onView(withId(android.R.id.text1)).check(matches(hasBackgroundColor(Color.parseColor("#ff000000"))))
+        onView(withId(R.id.border)).check(matches(hasBackgroundColor(Color.parseColor("#ff000000"))))
     }
 
     @Test
     fun testBinding_setBackground_Drawable() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setBackground(Background.of(ShapeDrawable(RectShape())))
         }
-        onView(withId(android.R.id.text1)).check(matches(isDrawableClassOf<ShapeDrawable>()))
+        onView(withId(R.id.border)).check(matches(isDrawableClassOf<ShapeDrawable>()))
     }
 
     @Test
     fun testBinding_setBackground_None() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .apply {
                     setBackgroundColor(Color.RED)
                     setBackground(Background.None)
                 }
         }
-        onView(withId(android.R.id.text1)).check(matches(noBackground()))
+        onView(withId(R.id.border)).check(matches(noBackground()))
     }
 
     @Test
     fun testBinding_setBackgroundTint_Res() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setBackgroundTint(Tint.of(android.R.color.tab_indicator_text))
         }
-        onView(withId(android.R.id.text1)).check(matches(withBackgroundTint { tint ->
+        onView(withId(R.id.border)).check(matches(withBackgroundTint { tint ->
             tint.defaultColor == Color.parseColor("#808080")
         }))
     }
@@ -158,10 +157,10 @@ class ViewBindingTest {
     @Test
     fun testBinding_setBackgroundTint_ColorStateList() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setBackgroundTint(Tint.of(ColorStateList.valueOf(Color.RED)))
         }
-        onView(withId(android.R.id.text1)).check(matches(withBackgroundTint { tint ->
+        onView(withId(R.id.border)).check(matches(withBackgroundTint { tint ->
             tint.defaultColor == Color.RED
         }))
     }
@@ -169,166 +168,166 @@ class ViewBindingTest {
     @Test
     fun testBinding_setBackgroundTint_None() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .apply {
                     backgroundTintList = ColorStateList.valueOf(Color.RED)
                     setBackgroundTint(Tint.none())
                 }
         }
-        onView(withId(android.R.id.text1)).check(matches(noBackgroundTint()))
+        onView(withId(R.id.border)).check(matches(noBackgroundTint()))
     }
 
     @Test
     fun testBinding_setContentDescription_Res() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setContentDescription(Text.of(android.R.string.ok))
         }
-        onView(withId(android.R.id.text1)).check(matches(withContentDescription("OK")))
+        onView(withId(R.id.border)).check(matches(withContentDescription("OK")))
     }
 
     @Test
     fun testBinding_setContentDescription_CharSequence() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setContentDescription(Text.of("test"))
         }
-        onView(withId(android.R.id.text1)).check(matches(withContentDescription("test")))
+        onView(withId(R.id.border)).check(matches(withContentDescription("test")))
     }
 
     @Test
     fun testBinding_setPaddingStart() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setPaddingStart(Dimension.px(1f))
         }
-        onView(withId(android.R.id.text1)).check(matches(withPaddingStart(1)))
+        onView(withId(R.id.border)).check(matches(withPaddingStart(1)))
     }
 
     @Test
     fun testBinding_setPaddingEnd() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setPaddingEnd(Dimension.px(2f))
         }
-        onView(withId(android.R.id.text1)).check(matches(withPaddingEnd(2)))
+        onView(withId(R.id.border)).check(matches(withPaddingEnd(2)))
     }
 
     @Test
     fun testBinding_setPaddingLeft() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setPaddingLeft(Dimension.px(3f))
         }
-        onView(withId(android.R.id.text1)).check(matches(withPaddingStart(3)))
+        onView(withId(R.id.border)).check(matches(withPaddingStart(3)))
     }
 
     @Test
     fun testBinding_setPaddingRight() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setPaddingRight(Dimension.px(4f))
         }
-        onView(withId(android.R.id.text1)).check(matches(withPaddingEnd(4)))
+        onView(withId(R.id.border)).check(matches(withPaddingEnd(4)))
     }
 
     @Test
     fun testBinding_setPaddingTop() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setPaddingTop(Dimension.px(5f))
         }
-        onView(withId(android.R.id.text1)).check(matches(withPaddingTop(5)))
+        onView(withId(R.id.border)).check(matches(withPaddingTop(5)))
     }
 
     @Test
     fun testBinding_setPaddingBottom() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setPaddingBottom(Dimension.px(6f))
         }
-        onView(withId(android.R.id.text1)).check(matches(withPaddingBottom(6)))
+        onView(withId(R.id.border)).check(matches(withPaddingBottom(6)))
     }
 
     @Test
     fun testBinding_setPadding() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setPadding(Dimension.px(10f))
         }
-        onView(withId(android.R.id.text1)).check(matches(withPadding(10)))
+        onView(withId(R.id.border)).check(matches(withPadding(10)))
     }
 
     @Test
     fun testBinding_setMinHeight() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setMinHeight(Dimension.px(10f))
         }
-        onView(withId(android.R.id.text1)).check(matches(hasMinHeight(10)))
+        onView(withId(R.id.border)).check(matches(hasMinHeight(10)))
     }
 
     @Test
     fun testBinding_setMinWidth() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setMinWidth(Dimension.px(10f))
         }
-        onView(withId(android.R.id.text1)).check(matches(hasMinWidth(10)))
+        onView(withId(R.id.border)).check(matches(hasMinWidth(10)))
     }
 
     @Test
     fun testBinding_setClickable() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setClickable(Bool.TRUE)
         }
-        onView(withId(android.R.id.text1)).check(matches(isClickable()))
+        onView(withId(R.id.border)).check(matches(isClickable()))
     }
 
     @Test
     fun testBinding_setLongClickable() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setLongClickable(Bool.TRUE)
         }
-        onView(withId(android.R.id.text1)).check(matches(isLongClickable()))
+        onView(withId(R.id.border)).check(matches(isLongClickable()))
     }
 
     @Test
     fun testBinding_setFocusable() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setFocusable(Bool.TRUE)
         }
-        onView(withId(android.R.id.text1)).check(matches(isFocusable()))
+        onView(withId(R.id.border)).check(matches(isFocusable()))
     }
 
     @Test
     fun testBinding_setEnabled() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setEnabled(Bool.FALSE)
         }
-        onView(withId(android.R.id.text1)).check(matches(IsNot(isEnabled())))
+        onView(withId(R.id.border)).check(matches(IsNot(isEnabled())))
     }
 
     @Test
     fun testBinding_setSoundEffectsEnabled() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setSoundEffectsEnabled(Bool.FALSE)
         }
-        onView(withId(android.R.id.text1)).check(matches(IsNot(isSoundEffectsEnabled())))
+        onView(withId(R.id.border)).check(matches(IsNot(isSoundEffectsEnabled())))
     }
 
     @Test
     fun testBinding_setVisibility_Visible() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setVisibility(Integer.wrap(View.VISIBLE))
         }
-        onView(withId(android.R.id.text1)).check(
+        onView(withId(R.id.border)).check(
             matches(
                 ViewMatchers.withEffectiveVisibility(
                     ViewMatchers.Visibility.VISIBLE
@@ -340,10 +339,10 @@ class ViewBindingTest {
     @Test
     fun testBinding_setVisibility_Invisible() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setVisibility(Integer.wrap(View.INVISIBLE))
         }
-        onView(withId(android.R.id.text1)).check(
+        onView(withId(R.id.border)).check(
             matches(
                 ViewMatchers.withEffectiveVisibility(
                     ViewMatchers.Visibility.INVISIBLE
@@ -355,10 +354,10 @@ class ViewBindingTest {
     @Test
     fun testBinding_setVisibility_Gone() {
         scenario.onFragment { fragment ->
-            fragment.requireView().findViewById<TextView>(android.R.id.text1)
+            fragment.requireView().findViewById<View>(R.id.border)
                 .setVisibility(Integer.wrap(View.GONE))
         }
-        onView(withId(android.R.id.text1)).check(
+        onView(withId(R.id.border)).check(
             matches(
                 ViewMatchers.withEffectiveVisibility(
                     ViewMatchers.Visibility.GONE
