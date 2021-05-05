22c58559464b2413ea001dcc594bffac48ad1f80
crimsonwoods <crimsonwoods.dev@gmail.com>
2021-05-05 18:29:31 +0900

    :fire: Removes unnecessary implementions from ShadowTextView

--- testing/src/test/java/net/crimsonwoods/easydatabinding/shadows/ShadowTextView.kt
+++ testing/src/test/java/net/crimsonwoods/easydatabinding/shadows/ShadowTextView.kt
@@ -1,18 +1,11 @@
 package net.crimsonwoods.easydatabinding.shadows
 
-import android.graphics.Paint
-import android.graphics.Typeface
-import android.text.TextPaint
 import android.widget.TextView
 import org.robolectric.annotation.Implementation
 import org.robolectric.annotation.Implements
 
 @Implements(TextView::class)
 open class ShadowTextView : org.robolectric.shadows.ShadowTextView() {
-    private val textPaint = TextPaint(TextPaint.ANTI_ALIAS_FLAG).apply {
-        typeface = Typeface.DEFAULT
-        textSize = 15f
-    }
     private var isElegantTextHeight = false
 
     @Implementation
@@ -23,20 +16,5 @@ open class ShadowTextView : org.robolectric.shadows.ShadowTextView() {
     @Implementation
     protected fun isElegantTextHeight(): Boolean {
         return isElegantTextHeight
-    }
-
-    @Implementation
-    protected fun getPaint(): Paint {
-        return textPaint
-    }
-
-    @Implementation
-    protected fun getFontVariationSettings(): String? {
-        return textPaint.fontVariationSettings
-    }
-
-    @Implementation
-    protected fun setFontVariationSettings(value: String?): Boolean {
-        return textPaint.setFontVariationSettings(value)
     }
 }
