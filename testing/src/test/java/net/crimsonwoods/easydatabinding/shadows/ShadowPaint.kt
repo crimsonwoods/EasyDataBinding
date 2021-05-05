605ee06312371a1dfbcf1eb983631d3464e990ef
crimsonwoods <crimsonwoods.dev@gmail.com>
2021-05-05 18:28:57 +0900

    :fire: Remove unnecessary shadow classes

--- testing/src/test/java/net/crimsonwoods/easydatabinding/shadows/ShadowPaint.kt
+++
@@ -1,8 +0,0 @@
-package net.crimsonwoods.easydatabinding.shadows
-
-import android.graphics.Paint
-import org.robolectric.annotation.Implements
-import org.robolectric.shadows.ShadowPaint
-
-@Implements(value = Paint::class, looseSignatures = true)
-open class ShadowPaint : ShadowPaint()
