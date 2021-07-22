c52832754c3efe68cb5db735c2d4dcfee2bbc73d
crimsonwoods <crimsonwoods.dev@gmail.com>
2021-07-22 21:35:27 +0900

    Adds more tests

--- testing/src/test/java/net/crimsonwoods/easydatabinding/models/FloatTest.kt
+++ testing/src/test/java/net/crimsonwoods/easydatabinding/models/FloatTest.kt
@@ -2,13 +2,16 @@ package net.crimsonwoods.easydatabinding.models
 
 import android.content.Context
 import android.content.res.Resources
+import android.os.Build
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 import net.crimsonwoods.easydatabinding.testing.R
 import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
 class FloatTest {
@@ -24,6 +27,16 @@ class FloatTest {
         assertEquals(0.0f, Float.of(R.dimen.zero).toFloat(resources))
         assertEquals(0.5f, Float.of(R.dimen.zero_point_five).toFloat(resources))
         assertEquals(1.0f, Float.of(R.dimen.one_point_zero).toFloat(resources))
+        assertEquals(0.5f, Float.of(R.dimen.half).toFloat(resources))
+        assertEquals(1.0f, Float.of(R.dimen.full).toFloat(resources))
+    }
+
+    @Config(sdk = [Build.VERSION_CODES.Q])
+    @Test
+    fun testToFloat_Res_Q() {
+        assertEquals(0.0f, Float.of(R.dimen.zero).toFloat(resources))
+        assertEquals(0.5f, Float.of(R.dimen.zero_point_five).toFloat(resources))
+        assertEquals(1.0f, Float.of(R.dimen.one_point_zero).toFloat(resources))
     }
 
     @Test
@@ -31,5 +44,14 @@ class FloatTest {
         assertEquals(0.0f, Float.of(0.0f).toFloat(resources))
         assertEquals(0.5f, Float.of(0.5f).toFloat(resources))
         assertEquals(1.0f, Float.of(1.0f).toFloat(resources))
+    }
+
+    @Config(sdk = [Build.VERSION_CODES.P, Build.VERSION_CODES.Q])
+    @Test
+    fun testFloat_Res_InvalidArgument() {
+        assertFailsWith<Resources.NotFoundException> { Float.of(R.dimen.one_dp).toFloat(resources) }
+        assertFailsWith<Resources.NotFoundException> { Float.of(R.dimen.one_sp).toFloat(resources) }
+        assertFailsWith<Resources.NotFoundException> { Float.of(R.dimen.one_px).toFloat(resources) }
+        assertFailsWith<Resources.NotFoundException> { Float.of(R.dimen.one_pt).toFloat(resources) }
     }
 }
