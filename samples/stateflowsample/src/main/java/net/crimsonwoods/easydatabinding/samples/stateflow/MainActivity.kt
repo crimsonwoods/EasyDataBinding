79f8a2be9ee367c96bcc4af366629cb792395c4d
crimsonwoods <crimsonwoods.dev@gmail.com>
2022-07-28 16:55:25 +0900

    Implement sample code using StateFlow and DataBinding

--- samples/stateflowsample/src/main/java/net/crimsonwoods/easydatabinding/samples/stateflow/MainActivity.kt
+++ samples/stateflowsample/src/main/java/net/crimsonwoods/easydatabinding/samples/stateflow/MainActivity.kt
@@ -1,11 +1,25 @@
 package net.crimsonwoods.easydatabinding.samples.stateflow
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import net.crimsonwoods.easydatabinding.samples.stateflow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        binding.root.setOnClickListener {
+            viewModel.update()
+        }
     }
 }
